7bfc85c75e2954899205da0c83f5f6611d989117
Abhijith P <abhijithp@Abhijiths-MacBook-Air.local>
2026-01-21 20:20:54 +0530

    updated settings.py

--- darul_fatheh_project/settings.py
+++ darul_fatheh_project/settings.py
@@ -79,26 +79,21 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Email Configuration
-
+# Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+
+# Standard Gmail Settings (Safe to hardcode)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True  # Force this to True for Gmail
+
+# Credentials (Must come from Render)
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-# EMAIL_HOST_USER = 'theofaber26@gmail.com' 
-
-
-# EMAIL_HOST_PASSWORD = 'qqsr tlud ygud onky'
-
+# Default Senders
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
+SERVER_EMAIL = EMAIL_HOST_USER
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
